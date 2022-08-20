c527bdb4e7820fb22c99b2b57ebf8d8fe6f1ee9b
Leslie Pino <contactlespino@gmail.com>
2022-08-20 18:05:36 +0800

    Added functionalities and styling.

---
+++ index.js
@@ -0,0 +1,11 @@
+copyEmail = () => {
+    navigator.clipboard.writeText('contactlespino@gmail.com');
+}
+
+skypeChat = () => {
+    window.open('https://join.skype.com/invite/uJR30ME55Dyq', '_blank');
+}
+
+contact = () => {
+    window.location.href='/contact.html';
+}
