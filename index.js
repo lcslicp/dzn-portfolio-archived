caa12feae54650e43443a72b885e86926ca713c5
Leslie Pino <contactlespino@gmail.com>
2022-08-27 13:06:09 +0800

    Fixed media queries.

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ skypeChat = () => {
 contact = () => {
     window.location.href='/contact.html';
 }
+
+burgerOpen = () => {
+    burgerMenu = document.querySelector('#burger-menu');
+
+    burgerMenu.style.visibility = 'visible';
+    burgerMenu.style.animation = 'slidein 1s ease-in-out';
+}
+
+burgerClose = () => {
+    burgerMenu = document.querySelector('#burger-menu');
+
+    burgerMenu.style.visibility = 'collapse';
+
+    burgerMenu.style.animation = 'slideout 1s ease-in-out';
+}
